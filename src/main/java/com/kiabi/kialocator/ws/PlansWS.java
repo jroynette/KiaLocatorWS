65eec0320484b9e6afbcfc25299268a3028b6b70
freecodedz <freecodedz@users.noreply.github.com>
2017-12-24 00:09:29 +0100

    feat: add cross for kia locator admin domain

--- src/main/java/com/kiabi/kialocator/ws/PlansWS.java
+++ src/main/java/com/kiabi/kialocator/ws/PlansWS.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.kiabi.kialocator.bo.Plan;
 import com.kiabi.kialocator.repository.PlanRepository;
 
-@CrossOrigin(origins = {"https://kia-locator-admin.herokuapp.com/","https://kialocator.herokuapp.com/"})
+@CrossOrigin(origins = "https://kia-locator-admin.herokuapp.com")
 @RestController
 public class PlansWS {
 
